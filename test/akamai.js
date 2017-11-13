9d90b36d703192e9f6492d8ccd311c6b422133cc
Ilja Strobbe <ilja@robonaut.be>
2017-11-13 12:24:11 +0100

    fix default hdnts token type value

--- test/akamai.js
+++ test/akamai.js
@@ -16,7 +16,7 @@ describe('Akamai', () => {
 			window: 600,
 			acl: '/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*',
 		});
-		expect(token).to.eql('st=1506351323~exp=1506351923~acl=/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*~hmac=328466b8743199db56f95cb059107e58');
+		expect(token).to.eql('exp=1506351923~acl=/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*~hmac=53534247b3af670da1f5ccac7889ead4');
 	});
 
 	it('gets query string', () => {
@@ -31,7 +31,20 @@ describe('Akamai', () => {
 			window: 600,
 			acl: '/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*',
 		});
-		expect(qs).to.eql('?token=st=1506351323~exp=1506351923~acl=/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*~hmac=328466b8743199db56f95cb059107e58');
+		expect(qs).to.eql('?token=exp=1506351923~acl=/wngg_VOK3DIVL0001D/media/wngg_VOK3DIVL0001D_nl_1280x720_28_1*~hmac=53534247b3af670da1f5ccac7889ead4');
+	});
+
+	it('generates auth token without salt', () => {
+		const akamai = new Akamai({
+			key: '026E7DD5A2FD47DDCA139190C4AB1A1F',
+			algorithm: 'sha256',
+			tokenName: 'hdnts',
+		});
+		const qs = akamai.getQueryString({
+			window: 600,
+			acl: '*/*',
+		});
+		// console.log('--------------->', qs);
 	});
 
 });
